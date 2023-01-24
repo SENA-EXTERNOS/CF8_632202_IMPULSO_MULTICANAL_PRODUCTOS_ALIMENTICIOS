827b19d0885b59f3e5fbe09883ad9d4529f75664
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-24 13:28:38 -0500

    Se vinculan los dos videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -21,7 +21,9 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen).my-3           
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/mZafXr-oPec" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          figcaption Video: La importancia de la calidad de los alimentos
+          
 </template>
 
 <script>
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -66,7 +66,8 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/VnkYJtRXTHs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          figcaption Video: Responsables de la calidad
 
     Separador 
     #t_1_2.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
